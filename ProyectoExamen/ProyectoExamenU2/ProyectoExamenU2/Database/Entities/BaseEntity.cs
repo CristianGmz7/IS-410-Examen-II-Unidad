930c5458e06e8f26b365200c8a1d08b4126f94cc
CristianGmz7 <cristiangomez.cagg78@gmail.com>
2024-07-26 19:42:25 -0600

    Primer_Avance_Examen

---
+++ ProyectoExamen/ProyectoExamenU2/ProyectoExamenU2/Database/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoExamenU2.Database.Entities;
+
+public class BaseEntity
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+}
---
+++ ProyectoExamen/ProyectoExamenU2/ProyectoExamenU2/Services/ClientsService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ProyectoExamenU2.Database;
+using ProyectoExamenU2.Services.Interfaces;
+
+namespace ProyectoExamenU2.Services;
+
+public class ClientsService : IClientsService
+{
+    private readonly ProyectoExamenU2Context _context;
+    private readonly IMapper _mapper;
+
+    public ClientsService(ProyectoExamenU2Context context, IMapper mapper)
+    {
+        this._context = context;
+        this._mapper = mapper;
+    }
+
+    //Metodo crear 
+}
